f8897977dd7ab8d76a0ee6cabe120e60996c7fa3
alanmox <alanmox441@gmail.com>
2026-07-04 16:01:42 +0300

    disable display_errors for production

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 
 require_once __DIR__ . '/../classes/Database.php';
 require_once __DIR__ . '/../classes/Encryption.php';
