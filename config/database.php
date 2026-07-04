0d2e7dcdeef7aa8202e7ad0f2df3b77f2eb1ffed
alanmox <alanmox441@gmail.com>
2026-07-04 15:39:42 +0300

    chore: ignore sensitive database configuration and standard project temporary files

--- config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'kibanda_umiza');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('ENCRYPTION_KEY', 'KibandaUmiza@2024!SecureKey');
-define('BASE_URL', 'http://localhost/kibanda-umiza');
